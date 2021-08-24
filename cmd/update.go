df5547eb551dddd8a9253f0c91519863fb878e82
Engin Diri <engin.diri@mail.schwarz>
2021-08-24 19:55:42 +0200

    Add module version support (#5)
    Signed-off-by: Engin Diri <engin.diri@mail.schwarz>

--- cmd/update.go
+++ cmd/update.go
@@ -2,13 +2,15 @@ package tfu
 
 import (
 	"fmt"
-	"github.com/briandowns/spinner"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/dirien/tfu/pkg/git"
 
 	"github.com/dirien/tfu/pkg/hcl"
 	"github.com/dirien/tfu/pkg/registry"
@@ -19,7 +21,6 @@ import (
 )
 
 func init() {
-
 	tfuCmd.AddCommand(updateCmd)
 	updateCmd.Flags().StringP("directory", "d", ".", "directory to check")
 	updateCmd.Flags().StringP("file", "f", "", "Single file")
@@ -28,7 +29,7 @@ func init() {
 
 var updateCmd = &cobra.Command{
 	Use:           "update",
-	Short:         "Updates the terrafrom provider",
+	Short:         "Updates the terraform provider and modules",
 	RunE:          runList,
 	SilenceUsage:  true,
 	SilenceErrors: true,
@@ -48,6 +49,9 @@ func runList(cmd *cobra.Command, _ []string) error {
 		return errors.Wrap(err, "failed to get 'dry-run' value.")
 	}
 	fmt.Println("")
+	if !git.CheckGitTokenIsSet() {
+		fmt.Println("🔴 GIT_TOKEN not set, skipping private module checks")
+	}
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.HideCursor = true
 	s.Prefix = "🔎 Start scanning for TF providers...  "
@@ -55,19 +59,19 @@ func runList(cmd *cobra.Command, _ []string) error {
 	s.Start()
 
 	var tfProvisioner []TFProvisioner
-
 	if len(file) > 0 {
-		tfProvisioner, err = inspectFileForOutdatedProvisioner(file, tfProvisioner, dryRun)
+		tfProvisioner, err = inspectFileForOutdatedProvisioner(file, dryRun)
 		if err != nil {
 			return err
 		}
 	} else if len(directory) > 0 {
 		err = filepath.Walk(directory, func(path string, info os.FileInfo, fileErr error) error {
 			if filepath.Ext(path) == ".tf" {
-				tfProvisioner, err = inspectFileForOutdatedProvisioner(path, tfProvisioner, dryRun)
+				provisionersPerFile, err := inspectFileForOutdatedProvisioner(path, dryRun)
 				if err != nil {
 					return err
 				}
+				tfProvisioner = append(tfProvisioner, provisionersPerFile...)
 			}
 			return nil
 		})
@@ -77,10 +81,15 @@ func runList(cmd *cobra.Command, _ []string) error {
 	}
 	s.Stop()
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"FILE", "PROVIDER", "VERSION", "REGISTRY VERSION", "UPDATE"})
+	table.SetHeader([]string{"FILE", "PROVIDER (P) / MODULE (M)", "USED VERSION", "LATEST VERSION", "UPDATABLE"})
+	table.SetAutoWrapText(false)
 	for _, provisioner := range tfProvisioner {
 		update := provisioner.OldVersion.LessThan(provisioner.NewVersion)
-		table.Append([]string{provisioner.Filename, provisioner.Provider, provisioner.OldVersion.String(), provisioner.NewVersion.String(), strconv.FormatBool(update)})
+		var color []tablewriter.Colors
+		if update {
+			color = []tablewriter.Colors{{tablewriter.Normal}, {tablewriter.Normal}, {tablewriter.FgHiWhiteColor, tablewriter.BgRedColor}, {tablewriter.Normal, tablewriter.BgGreenColor}}
+		}
+		table.Rich([]string{provisioner.Filename, provisioner.Provider, provisioner.OldVersion.String(), provisioner.NewVersion.String(), strconv.FormatBool(update)}, color)
 	}
 	table.SetBorder(false)
 	table.Render()
@@ -88,12 +97,12 @@ func runList(cmd *cobra.Command, _ []string) error {
 	return nil
 }
 
-func inspectFileForOutdatedProvisioner(file string, tfProvisioner []TFProvisioner, dryRun bool) ([]TFProvisioner, error) {
-	providers, err := hcl.NewHCLRequiredProvidersParser(file)
+func inspectFileForOutdatedProvisioner(file string, dryRun bool) ([]TFProvisioner, error) {
+	hclFile, err := hcl.NewHCLFileParser(file)
 	if err != nil {
 		return nil, err
 	}
-	tfProvisioner, err = checkProviderVersions(file, providers, dryRun, tfProvisioner)
+	tfProvisioner, err := checkProviderVersions(file, hclFile, dryRun)
 	if err != nil {
 		return nil, err
 	}
@@ -107,10 +116,69 @@ type TFProvisioner struct {
 	NewVersion *version.Version
 }
 
-func checkProviderVersions(filename string, providers *hcl.RequiredProviders, dryRun bool, tfProvisioner []TFProvisioner) ([]TFProvisioner, error) {
-	for _, provider := range providers.Providers {
+func checkProviderVersions(filename string, tfFile *hcl.TFFile, dryRun bool) ([]TFProvisioner, error) {
+	var tfProvisioner []TFProvisioner
+	// check for version in module block
+	if git.CheckGitTokenIsSet() {
+		for _, provider := range tfFile.Module {
+			repo := git.ParseGithubInfos(provider.Source)
+			if repo != nil {
+				latestVersion, err := repo.GetLatestVersion()
+				if err != nil {
+					return nil, err
+				}
+				if latestVersion != nil {
+					tfProvisioner = append(tfProvisioner, TFProvisioner{
+						Filename:   filename,
+						Provider:   fmt.Sprintf("%s (M)", provider.Source),
+						OldVersion: repo.Version,
+						NewVersion: latestVersion,
+					})
+					providerSourceBase := strings.SplitAfter(provider.Source, "ref=")[0]
+					newSourceBase := fmt.Sprintf("%s%s", providerSourceBase, latestVersion)
+					err = updateHCLFile(filename, provider.Source, newSourceBase, dryRun)
+					if err != nil {
+						return nil, err
+					}
+				}
+			}
+		}
+	}
 
-		registryProvider, err := registry.GetRegistryProvider(provider["source"])
+	// check for Terraform registry modules
+	for _, provider := range tfFile.Module {
+		if len(provider.Version) != 0 {
+			registryProvider, err := registry.GetRegistryDetails(provider.Source, registry.Modules)
+			if err != nil {
+				return tfProvisioner, err
+			}
+
+			oldV, err := version.NewVersion(provider.Version)
+			if err != nil {
+				return nil, err
+			}
+			newV, err := version.NewVersion(registryProvider.Version)
+			if err != nil {
+				return nil, err
+			}
+
+			tfProvisioner = append(tfProvisioner, TFProvisioner{
+				Filename:   filename,
+				Provider:   fmt.Sprintf("%s (M)", provider.Source),
+				OldVersion: oldV,
+				NewVersion: newV,
+			})
+
+			err = updateHCLFile(filename, fmt.Sprintf("version = \"%s\"", provider.Version), fmt.Sprintf("version = \"%s\"", registryProvider.Version), dryRun)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	// check required_provider in terraform block
+	for _, provider := range tfFile.Terraform.RequiredProviders.Providers {
+
+		registryProvider, err := registry.GetRegistryDetails(provider["source"], registry.Providers)
 		if err != nil {
 			return tfProvisioner, err
 		}
@@ -125,12 +193,12 @@ func checkProviderVersions(filename string, providers *hcl.RequiredProviders, dr
 		}
 		tfProvisioner = append(tfProvisioner, TFProvisioner{
 			Filename:   filename,
-			Provider:   provider["source"],
+			Provider:   fmt.Sprintf("%s (P)", provider["source"]),
 			OldVersion: oldV,
 			NewVersion: newV,
 		})
 
-		err = updateHCLFile(filename, provider["version"], registryProvider.Version, dryRun)
+		err = updateHCLFile(filename, fmt.Sprintf("version = \"%s\"", provider["version"]), fmt.Sprintf("version = \"%s\"", registryProvider.Version), dryRun)
 		if err != nil {
 			return nil, err
 		}
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/dirien/tfu
 go 1.16
 
 require (
-	github.com/briandowns/spinner v1.16.0 // indirect
+	github.com/briandowns/spinner v1.16.0
 	github.com/fatih/color v1.12.0
-	github.com/go-resty/resty/v2 v2.6.0 // indirect
-	github.com/hashicorp/go-version v1.3.0 // indirect
+	github.com/go-resty/resty/v2 v2.6.0
+	github.com/google/go-github/v38 v38.1.0
+	github.com/hashicorp/go-version v1.3.0
 	github.com/hashicorp/hcl/v2 v2.10.1
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.2.1
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
 )
